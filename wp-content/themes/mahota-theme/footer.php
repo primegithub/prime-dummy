987177db3032252ecc00f213d9f717d2a0c31b43
Prime Webdev <primewebdev@Primes-MacBook-Pro.local>
2016-03-04 14:26:59 +0800

    first commit

---
+++ wp-content/themes/mahota-theme/footer.php
@@ -0,0 +1,12 @@
+		
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div><!-- end of mastfoot -->
+        </div><!-- end of cover-container-->
+      </div><!-- end of site-wrapper-inner-->
+    </div><!--end of site-wrapper-->
+	<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ wp-content/themes/mahota-theme/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+          <div class="inner cover">
+            <h1 class="cover-heading">Mahota Sample Page.</h1>
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Learn more</a>
+            </p>
+          </div><!-- end of inner-cover -->
+<?php get_footer(); ?>
